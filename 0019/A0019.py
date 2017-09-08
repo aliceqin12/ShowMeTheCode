8c0ecbe133bc5a4f9962230aa364b13ff52deca0
olyzhang <geekworldzhang@163.com>
2017-09-08 12:21:30 +0800

    Add 0019 Update 0018

--- 0019/A0019.py
+++ 0019/A0019.py
@@ -5,33 +5,33 @@ from xml.dom.minidom import Document
 from collections import OrderedDict
 
 comment = '''
-            城市信息
+            数字信息
 '''
 
-def ReadExcelFileToDict(excelpath):
+def ReadExcelFileToList(excelpath):
     wb = xlrd.open_workbook(excelpath)
     ws = wb.sheets()[0]
 
-    city_dict = OrderedDict()
+    numbers_list = []
     for i in range(3):
-        id = ws.cell(i, 0).value
-        city = ws.cell(i, 1).value
-        city_dict[id] = city
+        temp_list = []
+        for j in range(3):
+            temp_list.append(int(ws.cell(i, j).value))
+        numbers_list.append(temp_list)
+    return numbers_list
 
-    return city_dict
-
-def writeDataToXml(city_dict, xmlpath):
+def writeDataToXml(numbers_list, xmlpath):
     doc = Document()
     root_node = doc.createElement('root')
     doc.appendChild(root_node)
 
-    stu_node = doc.createElement('city')
+    stu_node = doc.createElement('numbers')
     root_node.appendChild(stu_node)
 
     note_node = doc.createComment(comment)
     stu_node.appendChild(note_node)
 
-    dict_node = doc.createTextNode(json.dumps(city_dict, ensure_ascii=False, indent=4))
+    dict_node = doc.createTextNode(json.dumps(numbers_list, ensure_ascii=False, indent=4))
     stu_node.appendChild(dict_node)
 
     with open(xmlpath, "w", encoding='utf8') as file:
@@ -39,7 +39,7 @@ def writeDataToXml(city_dict, xmlpath):
         file.write(transform)
 
 if __name__ == '__main__':
-    excelpath = 'city.xls'
-    xmlpath = 'city.xml'
-    city_dict = ReadExcelFileToDict(excelpath)
-    writeDataToXml(city_dict, xmlpath)
+    excelpath = 'numbers.xls'
+    xmlpath = 'numbers.xml'
+    numbers_list = ReadExcelFileToList(excelpath)
+    writeDataToXml(numbers_list, xmlpath)
